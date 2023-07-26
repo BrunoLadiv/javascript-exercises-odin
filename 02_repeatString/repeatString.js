6a7da8dc7814992654bdadf9cd625948205904de
Bruno Vidal <brunokasprik@gmail.com>
2023-07-26 10:27:20 -0300

    exerc 2 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
-};
+const repeatString = function (string, times) {
+  if (times < 0) {
+    return 'ERROR'
+  }
+  let newString = ''
+  for (let index = 0; index < times; index++) {
+    newString += string
+  }
+  return newString
+}
 
 // Do not edit below this line
-module.exports = repeatString;
+module.exports = repeatString
+
+// console.log(repeatString('hey', 4))
